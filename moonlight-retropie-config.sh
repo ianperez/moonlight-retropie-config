2121f5d67a6707c32cf89a258cde3c6e37328c4c
Ian Perez <ianrobertperez@gmail.com>
2016-02-10 19:03:14 +0000

    Added path variable to easily change where game files are written and also now removes any existing games before the first write in case any games were removed

--- moonlight-retropie-config.sh
+++ moonlight-retropie-config.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-mkdir -p ~/RetroPie/roms/moonlight
+path=~/RetroPie/roms/moonlight
+mkdir -p $path
 
 start=0
 moonlight list $1 | while read line; do
   if [[ $line == 1.* ]]; then
+    rm $path/*.game
     start=1
   fi
   if [[ $start == 1 ]]; then
     name=${line:$(expr index "$line" " ")}
-    touch ~/RetroPie/roms/moonlight/"${name}".game
+    touch $path/"${name}".game
   fi
 done
 
